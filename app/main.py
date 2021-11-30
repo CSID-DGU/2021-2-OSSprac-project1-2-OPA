336401fe5a3a45cbdd739c208dfb6d5e20e069fe
Jay-the-notorious <57624475+Jay-the-notorious@users.noreply.github.com>
2021-11-30 14:32:21 +0900

    main_ :letter fix
    result.html : add <head> for styles

--- app/main.py
+++ app/main.py
@@ -8,17 +8,17 @@ def student():
     return render_template('main.html')
 
 
-@app.route('/detail', methods=['POST'])
+@app.route('/detail', methods = ['POST'])
 def detail():
     res = request.form.to_dict()
-    return render_template("detail.html", result=res)
+    return render_template("detail.html", result = res)
 
 
-@app.route('/result', methods=['POST'])
+@app.route('/result', methods = ['POST'])
 def result():
     res = request.form.to_dict()
-    return render_template("result.html", result=res)
+    return render_template("result.html", result = res)
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", debug=True, port=80)
+    app.run(host="0.0.0.0", debug = True, port = 80)
